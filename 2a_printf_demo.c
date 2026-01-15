66d3c8a423ca454c92f0d486afe9d16ee999006d
Moretti <72128066+2066295728@users.noreply.github.com>
2026-01-15 19:41:28 +0800

    Initial commit

---
+++ 2a_printf_demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("   *\n");
+    printf("=========================\n");
+    printf("This is a \"double quoted\" text. And here is a backslash\\. \n");
+    printf("==========================\n");
+    printf("Name\tAge\tGender\n");
+    printf("john\t22\tnew york\n");
+    return 0;
+}
