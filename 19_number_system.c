bbc178625c4f9a7e636119cace0a5d1630dd8c05
2066295728 <xly2066295728@outlook.com>
2026-02-21 17:15:16 +0800

    12

---
+++ 19_number_system.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	printf("500的八进制为%#o\n", 500);
+	printf("500的十六进制为%#x\n", 500);
+	printf("500的十六进制为%#X\n", 500);
+	printf("八进制764的十进制为%d\n", 0764);
+	printf("十六进制1f4的十进制为%d\n", 0x1f4);
+
+	return 0;
+}
